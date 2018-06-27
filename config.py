bfc65183c6f926538fba2c3cfb5488cfb4c1de28
MrMofu <mr_mofu@163.com>
2018-06-27 16:46:36 +0800

    login

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-#encoding: utf-8
+#encoding:utf-8
 import os
 from datetime import timedelta
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.urandom(24)
 PERMANENT_SESSION_LIFETIME = timedelta(days=5)
--- index.py
+++ index.py
@@ -1,13 +1,35 @@
-#encodig: utf-8
+#encodig:utf-8
 
-from flask import Flask
+from flask import Flask,request,session,redirect,url_for,render_template
+import config as config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route('/')
 def index():
-	return u'这是首页'
+	return render_template('index.html')
+
+# user模块
+'''
+login --  登录+注册页
+user  --  个人信息页
+'''
+
+# login 登录+注册
+@app.route('/login/', methods = ['GET', 'POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('/user/login.html')
+	else:
+		username = request.form.get('username')
+		password = request.form.get('password')
+		return redirect(url_for('user',userid=username))
+
+# user 个人信息页
+@app.route('/user/<userid>')
+def user(userid):
+	return userid
 
 if __name__ == '__main__':
 	app.run()
