099455d539e1639b0872606812735f63265f2d01
MrMofu <mr_mofu@163.com>
2018-06-28 18:00:07 +0800

    数据库设置

--- config.py
+++ config.py
@@ -10,10 +10,12 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=5)
 # 数据库配置 这里使用MYSQL
 DIALECT = 'mysql'
 DRIVER = 'mysqldb'
-USERNAME = 'root'
+USERNAME = 'root' # 用户名、密码根据各位本地mysql的设置而定
 PASSWORD = 'root'
 HOST = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'db_bignews'
 
 SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
