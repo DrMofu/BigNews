4b60cb1a569a1d94cee795bbab179cc0e7cf5863
MrMofu <mr_mofu@163.com>
2018-06-28 11:47:21 +0800

    bootstrap

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-from flask import Flask,request,session,redirect,url_for,render_template
-import config as config
+from flask import Flask,request,session,redirect,url_for,render_template,g
+import config
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -12,8 +12,9 @@ def index():
 
 # user模块
 '''
-login --  登录+注册页
-user  --  个人信息页
+login      --  登录+注册页
+user       --  个人信息页
+user_info  --  他人信息页
 '''
 
 # login 登录+注册
@@ -24,12 +25,37 @@ def login():
 	else:
 		username = request.form.get('username')
 		password = request.form.get('password')
-		return redirect(url_for('user',userid=username))
+		session['username'] = username
+		return redirect(url_for('user'))
 
 # user 个人信息页
+@app.route('/user/')
+def user():
+	if hasattr(g,'username'):
+		return g.username
+	else:
+		return redirect(url_for('login'))
+
+# user_info 他人信息页
 @app.route('/user/<userid>')
-def user(userid):
+def user_info(userid):
 	return userid
+
+# 钩子函数
+# before_request 在每次request前进行处理
+
+@app.before_request
+def get_info():
+	if session.get('username'):
+		g.username = session['username']
+
+'''
+# context_processor 对所有渲染文档，统一变量
+@app.context_processor
+def context_pro():
+	if session.get('username'):
+		return {'username':session.get('username')}
+'''
 
 if __name__ == '__main__':
 	app.run()
